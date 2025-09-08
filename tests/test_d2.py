b4e25bae998d0e4e59f1c135a4caf5f027112466
flibbi <francesco.libbi93@.com>
2025-09-08 11:37:32 +0200

    test derivatives

---
+++ tests/test_d2.py
@@ -0,0 +1,56 @@
+from ase import Atoms
+from ase.build import molecule
+import numpy as np
+from ase.calculators.emt import EMT
+from d2 import diff_2nd
+from d3_efficient import diff_3rd
+from d4_efficient import diff_4th
+
+def test_d2():
+    input_structure = 'final_result'
+    calculator = EMT()
+    what = 'dynamical_matrix'
+
+    diff_2nd(input_structure, calculator, what)
+
+    phi = np.load('phi.npy')
+    phi_test = np.load('phi_test.npy')
+    diff = np.linalg.norm(phi-phi_test)
+
+    print('2nd derivatives error', diff)
+    assert diff < 1e-8
+
+def test_d3():
+    input_structure = 'final_result'
+    calculator = EMT()
+    what = 'dynamical_matrix'
+
+    diff_3rd(input_structure, calculator, what)
+
+    chi = np.load('chi.npy')
+    chi_test = np.load('chi_test.npy')
+    diff = np.linalg.norm(chi-chi_test)
+
+    print('3rd derivatives error', diff)
+    assert diff < 1e-8
+
+
+def test_d4():
+    input_structure = 'final_result'
+    calculator = EMT()
+    what = 'dynamical_matrix'
+
+    diff_4th(input_structure, calculator, what)
+
+    psi = np.load('psi.npy')
+    psi_test = np.load('psi_test.npy')
+    diff = np.linalg.norm(psi-psi_test)
+
+    print('4th derivatives error', diff)
+    assert diff < 1e-8
+
+
+
+
+
+
